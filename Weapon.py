a56288f3ab9cde2a92dcde4736d71924e5b36dd1
Valeriy Karagaev <valeriy.karagaev@mail.ru>
2020-12-03 09:33:49 +0300

    перезарядка

--- Weapon.py
+++ Weapon.py
@@ -1,17 +1,18 @@
 from Characters import Terrorist, SWAT
 class Gun:
+    left_bullet = 0
     def shot(self, aim):
-        self.bullets_at_chamber -= self.bullet_per_shot
-        self.all_bullets -= self.bullet_per_shot
         if self.bullets_at_chamber < 0:
             print('ничего не происходит')
         else:
+            self.bullets_at_chamber -= self.bullet_per_shot
+            self.all_bullets -= self.bullet_per_shot
             print('Бах')
             print(f'Осталось {self.bullets_at_chamber} патронов')
-        return aim.health - self.damage
+            return aim.health - self.damage
 
     def reload(self):
-        self.all_bullets -= self.bullets_at_chamber
+        self.all_bullets -= Gun.left_bullet
         print('*Звук перезарядки*')
         print(f'Еще есть {self.all_bullets} патронов')
 
@@ -21,7 +22,9 @@ class Rifle(Gun):
         self.bullets_at_chamber = 30
         self.bullet_per_shot = 5
         self.all_bullets = 120
+        self.cost = 2700
 
 
-
-
+class Machinegun(Rifle):
+    def __init__(self):
+        super().__init__()
