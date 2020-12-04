4499209c93cf3179c638e131b8423c3cbf7bc2df
Valeriy Karagaev <valeriy.karagaev@mail.ru>
2020-12-04 13:45:26 +0300

    final version (0.0)

--- Characters.py
+++ Characters.py
@@ -1,11 +1,16 @@
+import Weapon
+from Weapon import weapon_list
+
+
 class Character:
     def __init__(self):
         self.health = 100
         self.armor = 0
-        self.money = 16000
+        self.money = 6000
         self.height = 1
         self.speed = 2
         self.noise = 1
+        self.ammunition = 0
 
     def moving(self):
         if self.height == 0.5:
@@ -36,19 +41,31 @@ class Character:
         comands_list = ['да', 'нет', 'пойдем', 'стой здесь']
         print(comands_list[number])
 
+    def buy_weapon(self):
+        print('Выберите оружие')
+        for item in weapon_list:
+            print(f'{item.name} стоит {item.cost}')
+        choice = input('введите название оружия\n')
+        for item in weapon_list:
+            if choice == item.name:
+                self.money -= item.cost
+                self.ammunition = item
+
+
+    def shooting(self, aim):
+        if self.ammunition == 0:
+            print('надо купить оружие')
+        else:
+            self.ammunition.shot()
+            aim.health -= self.ammunition.damage
+
 
 class SWAT(Character):
-    swat_counter = 0
     def __init__(self):
-        SWAT.swat_counter += 1
-        if SWAT.swat_counter > 5:
-            print('Игроков спецназа слишком много')
-        else:
-            super().__init__()
-            self.weapon = 'SWAT'
-            self.defuse_ability = 0
-            print('Я - солдат, я не спал пять лет...')
-
+        super().__init__()
+        self.weapon = 'SWAT'
+        self.defuse_ability = 0
+        print('Я - солдат, я не спал пять лет...\n')
 
     def defuse(self):
         if self.defuse_ability == 0:
@@ -61,9 +78,16 @@ class Terrorist(Character):
     def __init__(self):
         super().__init__()
         self.weapon = 'Ter'
-        print('Бомбим,чувачки')
+        print('Бомбим,чувачки\n')
 
     def plant_bomb(self):
         print('Бомба установлена')
 
 
+# a = SWAT()
+# t = Terrorist()
+# a.buy_weapon()
+# # print(a.money)
+# # print(a.ammunition)
+# a.shooting(t)
+# # print(t.health)
