f7b030feb0aa9a1bc3e2e918d3434b567d9cd702
Valeriy Karagaev <valeriy.karagaev@mail.ru>
2020-12-07 17:10:34 +0300

    final version (0.1)

--- Characters.py
+++ Characters.py
@@ -3,6 +3,7 @@ from Weapon import weapon_list
 
 
 class Character:
+    #создает игрока и дает ему базовые функции
     def __init__(self):
         self.health = 100
         self.armor = 0
@@ -13,6 +14,7 @@ class Character:
         self.ammunition = 0
 
     def moving(self):
+        # иммитирует перемещение игрока по карте
         if self.height == 0.5:
             print('Присел и крадусь к противнику')
         elif self.noise == 0:
@@ -21,27 +23,28 @@ class Character:
             print('Бегу')
 
     def sit(self):
+        # меняет положение игрока на положение сидя
         self.height = 0.5
         self.speed = 1
         self.noise = 0
         print('так меня не видно из-за укрытия')
 
     def stand(self):
+        # меняет положение игрока на положение стандартное
         self.height = 1
         self.speed = 2
         self.noise = 1
         print('ничто меня не остановит ')
 
     def sneak(self):
+        # делает перемещение бесшумным
         self.speed = 1
         self.noise = 0
         print('надеюсь, меня не заметят')
 
-    def voice_command(self, number):
-        comands_list = ['да', 'нет', 'пойдем', 'стой здесь']
-        print(comands_list[number])
 
     def buy_weapon(self):
+        # выбирает оружие из списка доступных и вооружается им
         print('Выберите оружие')
         for item in weapon_list:
             print(f'{item.name} стоит {item.cost}')
@@ -50,24 +53,31 @@ class Character:
             if choice == item.name:
                 self.money -= item.cost
                 self.ammunition = item
+                print('Осталось', self.money)
 
 
     def shooting(self, aim):
+        # иммитация стрельбы
+        # aim - указывает на игрока, по которому будет произведен выстрел
         if self.ammunition == 0:
             print('надо купить оружие')
         else:
             self.ammunition.shot()
             aim.health -= self.ammunition.damage
 
+    def reloading(self):
+        self.ammunition.reload()
+
 
 class SWAT(Character):
+    # один из типов игроков, может обезвреживать бомбы
     def __init__(self):
         super().__init__()
-        self.weapon = 'SWAT'
         self.defuse_ability = 0
         print('Я - солдат, я не спал пять лет...\n')
 
     def defuse(self):
+        #иммитация обезвреживания бомы
         if self.defuse_ability == 0:
             print('обезвреживаю бомбу, справлюсь за 10 сек')
         else:
@@ -75,19 +85,12 @@ class SWAT(Character):
 
 
 class Terrorist(Character):
+    # один из типов игроков, может обезвреживать бомбы
     def __init__(self):
         super().__init__()
-        self.weapon = 'Ter'
         print('Бомбим,чувачки\n')
 
     def plant_bomb(self):
         print('Бомба установлена')
 
 
-# a = SWAT()
-# t = Terrorist()
-# a.buy_weapon()
-# # print(a.money)
-# # print(a.ammunition)
-# a.shooting(t)
-# # print(t.health)
--- Weapon.py
+++ Weapon.py
@@ -1,5 +1,6 @@
 
 class Gun:
+
     left_bullet = 0
     def __init__(self,name):
         self.name = name
